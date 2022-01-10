e126842a701c5e133319330a15660f97ed3dbdbc
antonkurin <antonkurin1@gmail.com>
2022-01-10 19:44:14 +0300

    Initial Commit

---
+++ DepartmentProjectInheritance/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DepartmentProjectInheritance
+//
+//  Created by Антон Курин on 10.01.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
